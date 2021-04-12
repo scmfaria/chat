e5d5125acc2b277cb2e1c44e61acbe4a71efe9f1
scmfaria <sara.moura.faria@gmail.com>
2021-04-12 19:03:26 -0300

    add cloud firestore and get informations

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,15 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, i) => Container(
-          padding: EdgeInsets.all(10),
-          child: Text('Funcionou!!'),
-        ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('chat').snapshots(),
+        builder: (ctx, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = snapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, i) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[i]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance.collection('chat').add({
+            'text': 'Adicionando manualmente',
+          });
+        },
       ),
     );
   }
